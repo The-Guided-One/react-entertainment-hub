9e676184d63ff98fdcfced1b6f21914b021d0614
The Guided One <ahmmasum98@gmail.com>
2021-09-26 02:54:01 +0600

    Update Trending.js
    1srt  commit

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -10,7 +10,7 @@ const Trending = () => {
 
   const fetchTrending = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+      `https://api.themoviedb.org/3/trending/all/day?api_key=fa852af59236970406a571e248ebda0a&page`
     );
 
     setContent(data.results);
